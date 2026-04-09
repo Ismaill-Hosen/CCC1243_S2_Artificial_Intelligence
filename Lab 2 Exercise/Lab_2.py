bb6f5304b9a54bfddb05b1521a617fa9ef5c1618
Ismailhosen342 <ismailas564@gmail.com>
2026-04-09 16:16:44 +0800

    Add lab-2

---
+++ Lab 2 Exercise/Lab_2.py
@@ -0,0 +1,31 @@
+# Question-1
+
+subject="Data Science"
+print(subject[0] ,subject[5])
+
+
+#Question-2
+
+word="Artificial"
+print(word[0:3] , word[4:])
+
+ #Question-3
+ 
+message = " hello world "
+message = message.strip()
+print(message)
+message = message.capitalize()
+print(message)
+message = message.replace("world", "AI")
+print(message)
+
+# Question-4
+ai_models=["ChatGPT", "Gemini"]
+print(ai_models[1])
+ 
+#question-5
+ 
+names = ["Alice", "Bob", "Charlie"]
+names.append("David")
+names[0] = "Zack"
+print(len(names))
