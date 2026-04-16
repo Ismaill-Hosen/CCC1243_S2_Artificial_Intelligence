35bdcb2a758d215e6deb69e2e4b68c22bd8b31ed
Ismailhosen342 <ismailas564@gmail.com>
2026-04-16 15:20:46 +0800

    Compete lab-3

---
+++ Lab 3 Exercise/Lab 3.py
@@ -0,0 +1,62 @@
+
+
+# Question-1: Write the Python code to create a list named fruits with the strings "apple", "banana", and "cherry". Then, write the code to add "orange" to this list
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits.append("orange")
+# print(fruits)
+
+# question-2: Write the exact code to create a tuple containing only the single string "hello" and assign it to the variable single
+# single = ("hello",)
+# print(single)
+# print(type(single))
+
+
+# Question-3:
+# Create a dictionary named student with the keys "name" (value: "Ali") and "age" (value: 20). Then, write the print statement to output only the student's name.
+
+# student = {"name": "Ali", "age": 20}
+# print(student["name"])
+
+
+
+# Question-4:Start with the code:tasks = ["Read", "Write"] Write the code to add "Sleep" to the end of the list. Then, using indexing, write the code to update "Read" to "Study".
+
+# tasks = ["Read", "Write"]
+# tasks.append("Sleep")
+# tasks[0] = "Study"
+# print(tasks)
+
+# Question-5: Write the code to define two sets: A = {1, 2, 3} and B = {3, 4, 5} Next, write the statement to print only the numbers that exist in both sets.
+
+# A = {1, 2, 3}
+# B = {3, 4, 5}
+# intersection = A.intersection(B)
+# print(intersection)
+
+#Question-6:Given the code: info = {"name": "Zara", "course": "AI"} Write the Python statements to change the course to "Data Science" and add a new key "grade" with the value "A".
+
+# info = {"name": "Zara", "course": "AI"}
+# info["course"] = "Data Science"
+# info["grade"] = "A"
+# print(info)
+
+#Question-7:Given the nested list:matrix = [[1, 2], [3, 4]] Write the exact print statement using indexing to output the number 3
+# matrix = [[1, 2], [3, 4]]
+# print(matrix[1][0])
+
+
+#Question-8:Start with: numbers = [1, 2, 2, 3, 4, 4, 5] unique_numbers = [] Write a for loop combined with an if statement to append only the unique numbers to unique_numbers.
+
+# numbers = [1, 2, 2, 3, 4, 4, 5]
+# unique_numbers = []
+# for number in numbers:
+#     if number not in unique_numbers:
+#         unique_numbers.append(number)
+# print(unique_numbers)
+
+# Question-9:Create a dictionary named translations that maps the key "hello" to "hola" and "goodbye" to "adiós". Write the code to extract and print the translation for "hello".
+
+# translations = {"hello": "hola", "goodbye": "adiós"}
+# print(translations["hello"])
+
